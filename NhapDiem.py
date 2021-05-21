d34fda53e9af410fb533b85bd36210e612e481b9
hauphanlvc <hauphanlvc@gmail.com>
2021-05-22 00:59:06 +0700

    commit

--- NhapDiem.py
+++ NhapDiem.py
@@ -1,15 +1,69 @@
+import io,os,sys
+from decimal import Decimal
+# input = io.BytesIO(os.read(0,os.fstat(0).st_size)).readline
+sys.stdin = open("Inputnhapdiem.txt","r")
+sys.stdout = open("Outputnhapdiem.txt","w")
 n = int(input())
 HeSo = []
 for i in range(n):
     x = int(input())
-    HeSo.append(x/100)
-i = int(input())
-GiaTriNhoNhat = int(i/n)
+    HeSo.append(x)
+Diem = float(input())
+Diem*=10000
+
+GiaTriNhoNhat = int(Diem/n)
 res = []
-i = GiaTriNhoNhat
-x = 10 
-while x >= 0 :
+
+def check(S,Diem,HeSo):
+    ok = 0 
     for i in range(n):
-        for x in range(GiaTriNhoNhat,10,0.25):
-        
+        ok += S[i]*HeSo[i]
     
+    # print(abs(ok- Diem))
+    if Diem > ok and Diem - ok <= 500 :
+        return True
+    elif Diem < ok and ok - Diem <= 490 :
+        return True
+    elif Diem== ok :
+        return True
+    return False
+def Print(S):
+    for i in S:
+        if abs(i - int((i/100))* 100) ==0 :
+            print(int(i/100),end=' ')
+        else :
+            print((i/100),end=' ')
+        
+def BackTrack(i,S,n,Diem,HeSo):
+    if i == n :
+        if check(S,Diem,HeSo):
+            Print(S)
+            print()
+            return 
+            # ok = 0 
+            # for i in range(len(S)):
+            #     ok += S[i]*HeSo[i]
+            # print("ok = ",ok)
+            # print("Diem = ",Diem)
+            # print(abs(ok - Diem))
+            # print("-------")
+
+    else :
+        for x in range(1,41):
+             
+            
+            d = x*25*HeSo[i]
+            for j in range(i):
+                d+= S[j]*HeSo[j]
+            if d - Diem > 600 : return
+            conlai = 0 
+            for j in range(i+1,n):
+                conlai+=1000*HeSo[j]
+            if Diem - d - conlai > 500:
+                pass
+            else :
+                S.append(x*25)
+                BackTrack(i+1,S,n,Diem,HeSo)
+                S.pop()
+
+BackTrack(0,[],n,Diem,HeSo)
--- OChu.py
+++ OChu.py
@@ -1,57 +1,65 @@
-# import io,os,sys
-# # input = io.BytesIO(os.read(0,os.fstat(0).st_size)).readline
-# sys.stdin = open("OChu.txt","r")
-# sys.stdout = open("OChu_1.txt","w")
+import io,os,sys
+
+# # # input = io.BytesIO(os.read(0,os.fstat(0).st_size)).readline
+sys.stdin = open("OChu.txt","r")
+sys.stdout = open("OChu_1.txt","w")
+
+
+
 String = input()
 a = []
 while True:
-    x = input()
-    if x[0]=='.':
+    x = (input())
+    if x[0]==".":
         break
     else :
-        a.append(x)
-Start = []
-for i in range(len(a)):
-    for j in range(len(a[0])):
-        if a[i][j]==String[0]:
-            Start.append((i,j))
-dx = [0,1,0,-1]
-dy = [1,0,-1,0]
-DoDai= 0 
-Check = 1
-while  DoDai < len(Start):
-    x, y = Start[DoDai][0],Start[DoDai][1]
-    i = 1 
-    DoDai +=1
-    DanhDau = [[0]*len(a[0])] * len(a)
-    DanhDau[x][y] = 1
-    while i < len(String):
-        k = 0 
-        while k < 4:
-            New_x = x + dx[k]
-            New_y = y + dy[k]
-            if New_x >=0 and New_y >= 0 and New_x < len(a[0]) and New_y < len(a) and DanhDau[x][y] == 0  :
-                if a[New_x][New_y] == String[i]  : 
-                    i+=1
-                    print("{} ----- {} -----".format(x,y))
-                    print("_________" + str(i))
-                    DanhDau[New_x][New_y]=1
-                    x = New_x
-                    y = New_y
-                    break
+        a.append(list(x))
+def Xuli(a,String):
+    Start = []
+    for i in range(len(a)):
+        for j in range(len(a[0])):
+            if a[i][j]==String[0]:
+                Start.append((i,j))
+    dx = [0,1,0,-1,-1,1,-1,1]
+    dy = [1,0,-1,0,-1,1,1,-1]
+    DoDai= 0 
+    Check = 1
+    while  DoDai < len(Start):
+        x, y = Start[DoDai][0],Start[DoDai][1]
+        stack = []
+        stack.append((x,y))
+        path = []
+        path.append((x,y))
+        kq =[]
+        while len(stack) !=0 :
+            s = stack.pop()
+            if len(path) == len(String): 
+                print("true")
+                print(*path)
+                print(String)
+                return
+            VT = []
+            for i in range(8):
+                VT1 = s[0] + dx[i]
+                VT2 = s[1] + dy[i]
+                if 0<= VT1 and VT1<len(a) and 0<=VT2 and VT2<len(a[0]) :
+                    VT.append((VT1,VT2))
+            while VT :
+                x = VT.pop()
                 
-            k+=1
-            if k ==3 : 
-            # print("False")
-                Check = 0
-            # exit()
-            break
-    if Check == 1 :
-        print("True")
-        exit()
+                if a[x[0]][x[1]] == String[len(path)] and  x not in path:
+                    path.append(x)
+                    stack.append(x)
+                    break
 
-if Check == 0 :
-    print("False")
+        DoDai+=1
+
+    print("false")
+
+Xuli(a,String)
+        
+
+
                     
 # ABCB
 # ABCE
